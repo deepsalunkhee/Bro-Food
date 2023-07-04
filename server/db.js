ea9903774d68c6f30a27cfea5192c669e89e494a
deepsalunkhee <99858248+deepsalunkhee@users.noreply.github.com>
2023-07-04 16:47:38 +0530

    d3

--- server/db.js
+++ server/db.js
@@ -2,13 +2,31 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 const MONGODB_URL = process.env.MONGO_URL;
 
-mongoose.set('strictQuery', false); 
+mongoose.set('strictQuery', false);
 
 const connectToDatabase = async () => {
   try {
     await mongoose.connect(MONGODB_URL, { useNewUrlParser: true });
-
     console.log('Connected to the database');
+
+    const fetched_data = await mongoose.connection.db.collection("food_items");
+    fetched_data.find({}).toArray(async function (err, data) {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      
+      const foodCatagory = await mongoose.connection.db.collection("foodCatagory");
+      foodCatagory.find({}).toArray(function (err, catData) {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        
+        global.food_items = data;
+        global.foodCatagory = catData;
+      });
+    });
 
     return mongoose.connection;
   } catch (error) {
@@ -18,4 +36,3 @@ const connectToDatabase = async () => {
 };
 
 module.exports = connectToDatabase();
-
